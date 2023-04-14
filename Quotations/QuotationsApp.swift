6c715e3546874d07f93bcb24bbc80387cadc5f5e
Tom <twu@lcs.on.ca>
2023-04-14 14:58:45 -0400

    Initial Commit

---
+++ Quotations/QuotationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuotationsApp.swift
+//  Quotations
+//
+//  Created by Tom Wu on 2023-04-14.
+//
+
+import SwiftUI
+
+@main
+struct QuotationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
